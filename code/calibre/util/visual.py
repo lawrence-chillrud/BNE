75d71514e85235f1fb36f002537727e80bc74081
lawrence-chillrud <lgc2139@columbia.edu>
2020-12-09 14:18:29 -0800

    Added missing dependency make_color_norm.

--- code/calibre/util/visual.py
+++ code/calibre/util/visual.py
@@ -20,6 +20,44 @@ import seaborn as sns
 
 from matplotlib.colors import BoundaryNorm
 
+def make_color_norm(color_data, method="percentile"):
+    """Makes color palette norm for heatmap plots.
+
+    Args:
+        color_data: (np.ndarray or list) Either a single numpy array or
+            a list of numpy array that records numeric values to adjust
+            color map to.
+        method: (str) The name of method to compute norm values:
+            percentile: Adjust norm to the raw percentile of color_data.
+            residual: Adjust norm to the symmetric range of
+                [-min(abs(data)), -max(abs(data))].
+                Color norm values will space out evenly in between the range.
+            residual_percentile: Similar to 'residual'.
+                But color norm values will be adjusted with respect to the
+                percentile of abs(data).
+
+    Returns:
+        (matplotlib.colors.BoundaryNorm) A color norm object for color map
+            to be passed to a matplotlib.pyplot function.
+    """
+    if isinstance(color_data, list):
+        color_data = np.concatenate(color_data)
+
+    if method == "percentile":
+        levels = np.percentile(color_data,
+                               np.linspace(0, 100, 101))
+    elif method == "residual":
+        abs_max = np.max(np.abs(color_data))
+        levels = np.linspace(-abs_max, abs_max, 101)
+    elif method == "residual_percentile":
+        abs_levels = np.percentile(np.abs(color_data),
+                                   np.linspace(0, 100, 101))
+        levels = np.sort(np.concatenate([-abs_levels, abs_levels]))
+    else:
+        raise ValueError("Method {} is not supported".format(method))
+
+    return BoundaryNorm(levels, 256)
+
 def posterior_heatmap_2d(plot_data, X,
                          X_monitor=None,
                          cmap='inferno_r',
